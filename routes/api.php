2a4c9e1e2b72bed4f628ea963a5f29a00d3331bd
Roger Melich <rogermelich@gmail.com>
2016-12-01 21:05:51 +0100

    start php artisan make:entity and crear route and api route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group(['prefix' => 'api/v1.0', 'middleware' => ['auth.api']], function () {
+    Route::resource('enrollment', 'EnrollmentsController');
+});
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('enrollment', 'EnrollmentsController');
+});
+
